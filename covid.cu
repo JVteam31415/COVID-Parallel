1f9d1e3b2a878fd79872d1da947f706fcedabff8
David Dawnkaski <david.dawnkaski@gmail.com>
2020-04-18 21:29:45 -0400

    aimos test

--- covid.cu
+++ covid.cu
@@ -49,13 +49,31 @@ static inline void covid_initMaster(unsigned int pop_size, size_t world_width, s
 	cudaMallocManaged((void**)world, g_worldHeight*g_worldWidth*depth*sizeof(person));
 	g_world = (person (**)[depth]) world;
 
+	for (int i=0; i<g_popSize; ++i) {
+		int x = rand() % (g_worldWidth + 1);
+		int y = rand() % (g_worldHeight + 1);
+
+		g_population[i].x = x;
+		g_population[i].y = y;
+		g_population[i].day_infected = NULL;
+		g_population[i].R = 0;
+		g_population[i].sate = 0;
+		g_population[i].symptoms = false;
+	}
 
 
-	for (int i <)
+
 }
 
 int main(int argc, char *argv[]) {
-	unsigned int pop_size, world_width, world_height, infection_radius, infection_chance;
+	unsigned int pop_size, world_width, world_height, infection_radius, infection_chance, days;
+
+	pop_size = atoi(argv[1]);
+	world_width = atoi(argv[2]);
+	world_height = atoi(argv[3]);
+	infection_radius = atoi(argv[4]);
+	days = atoi (argv[4]);
+	unsigned int timesteps = days*24;
 
 	srand(time(0));
 	covid_initMaster(pop_size, world_width, world_height);
